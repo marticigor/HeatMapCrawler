c45acc84fda6b40976145b59e6734590e24c929c
radim <radvecerek@yahoo.co.uk>
2017-11-07 19:21:13 +0100

    mini tweaks

--- src/beans/RunnerBean.java
+++ src/beans/RunnerBean.java
@@ -8,7 +8,7 @@ public class RunnerBean implements I_KickStart{
 	
 	// TODO but how...
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	public static final String NAME = "san_francisco_bay_south_cycling_14";
+	public static final String NAME = "test_7";
 	public static final String TABLE_SHOTS = NAME + "_shots";
 	public static final String TABLE_NODES = NAME + "_nodes";
 	public static final String TABLE_ADJACENTS = NAME + "_adjacents";
